9f32468232953c686d81fc5a0fa32320206cd8cc
Will Cooley <william.cooley@gmail.com>
2024-02-28 14:57:29 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -8,19 +8,107 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User(
+            username=json.get('username'),
+            image_url=json.get('image_url'),
+            bio=json.get('bio')
+        )
 
+        user.password_hash = json['password']
+
+        try: 
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+
+            response_data = {
+                'id': user.id,
+                'username': user.username,
+                'image_url': user.image_url,
+                'bio': user.bio
+            }
+            return response_data, 201
+        
+        except IntegrityError:
+            error = {'error': 'invalid input'}
+            return error, 422
+        
+        
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id=session['user_id']
+        try:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        except:
+            error = {'error': 'not authorized'}
+            return error, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        try:
+            username = request.get_json()['username']
+            user = User.query.filter(User.username == username).first()
+            password = request.get_json()['password']
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+        except:
+            error = {'error': 'not authorized'}
+            return error, 401
+            
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        user_id=session['user_id']
+        if user_id:
+            session['user_id'] = None
+            return {}, 204
+        else:
+            error = {'error': 'not authorized'}
+            return error, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        user_id=session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            recipes = [recipe.to_dict() for recipe in user.recipes]
+            print(user)
+            print(recipes)
+            return recipes, 200
+        else:
+            error = {'error': 'not authorized'}
+            return error, 401
+        
+    def post(self):
+        user_id=session['user_id']
+        if user_id:
+            try:
+                recipe_data = request.get_json()
+                new_recipe = Recipe(
+                    user_id=user_id,
+                    title=recipe_data.get('title'),
+                    instructions=recipe_data.get('instructions'),
+                    minutes_to_complete=recipe_data.get('minutes_to_complete')
+                )
+                db.session.add(new_recipe)
+                db.session.commit()
+
+                return new_recipe.to_dict(), 201
+
+            except:
+                error={'error': 'not a valid recipt'}
+                return error, 422
+        else:
+            error = {'error': 'not authorized'}
+            return error, 401
+
+
+        
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/testing/app_testing/app_test.py
+++ server/testing/app_testing/app_test.py
@@ -342,7 +342,7 @@ class TestRecipeIndex:
             response_json = response.get_json()
             
             with client.session_transaction() as session:
-                
+                # breakpoint()
                 new_recipe = Recipe.query.filter(Recipe.user_id == session['user_id']).first()
 
             assert response_json['title'] == new_recipe.title
